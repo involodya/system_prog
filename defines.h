531e3d97bc32f059698d7d4f5a412f47d980decc
vovans32 <kpvcha4@yandex.ru>
2021-09-20 12:11:27 +0300

    master branch normalized

--- defines.h
+++
@@ -1,4 +0,0 @@
-#define SS_INF_ROOTS -1         // code for an infinite number of solutions
-#define NUMBER_OF_TESTS 1      // number of unittest
-
-#define _TEST
--- unittest.h
+++
@@ -1,5 +0,0 @@
-void TestLaunch ();
-
-void TestGenerate(double *a, double *b, double *c, int n);
-
-void TestCheck(double x1, double x2, int n);
