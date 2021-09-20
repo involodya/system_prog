e119870240161b6a121abd83a0a4f186e1eba58a
vovans32 <kpvcha4@yandex.ru>
2021-09-20 12:06:54 +0300

    Onegin init, Vers 0.0

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 //-------------------------------------------------
-//! The squire equations solver programme
-//!     @version Vers 1.0
-//!     @date 17. 09. 2021
-//!     @brief Solving task #1 for the course "Professional Programming"
+//! Onegin program
+//!     @version Vers 0.0
+//!     @date 20. 09. 2021
+//!     @brief Solving task #2 for the course "Professional Programming"
 //-------------------------------------------------
 
 #include <stdio.h>
@@ -10,53 +10,10 @@
 #include "defines.h"
 #include "solve.h"
 
-#ifdef _TEST
-#include "unittest.h"
-#endif
-
 
 int main() {
 
-    printf("# Square equation solver\n"
+    printf("# Onegin\n"
            "# @involodya, 2021\n\n");
-    printf("# Enter a, b, c: ");
-
-    double a = 0, b = 0, c = 0, n = 1;
-
-#ifdef _TEST
-    n = NUMBER_OF_TESTS;
-    TestLaunch();
-#endif
-
-    for (int i = 0; i < n; ++i) {
-#ifdef _TEST
-        TestGenerate(&a, &b, &c, i);
-#else
-        scanf("%lg %lg %lg", &a, &b, &c);
-#endif
-        double x1 = 0, x2 = 0;
-        int nRoots = SolveSquare(a, b, c, &x1, &x2);
-#ifdef _TEST
-        TestCheck(x1, x2, i);
-#endif
-        switch (nRoots) {
-            case 0:
-                printf("No roots\n");
-                break;
-            case 1:
-                printf("x = %lg\n", x1);
-                break;
-            case 2:
-                printf("x1 = %lg, x2 = %lg\n", x1, x2);
-                break;
-            case SS_INF_ROOTS:
-                printf("Any number");
-                break;
-            default:
-                printf("main(): ERROR: nRoots = %d\n",
-                       nRoots);
-                return 1;
-        }
-    }
 
 }
